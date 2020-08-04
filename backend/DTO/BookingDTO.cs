c9e914c770c48af11dd73c12260b6f7b5333ee78
Darius Haș <has.darius17@yahoo.com>
2020-08-04 13:52:51 +0300

    Added DTO class for Booking, updated Controller to work with DTOs rather that directly on db models. Need to update PutBooking() method to only update certain fields rather than all of them.

---
+++ backend/DTO/BookingDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaApp.Models;
+
+namespace CinemaApp.DTO
+{
+    public class BookingDTO
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+
+        public TimeSlot TimeSlot { get; set; }
+
+        public List<Seat> Seats { get; set; }
+
+    }
+}
