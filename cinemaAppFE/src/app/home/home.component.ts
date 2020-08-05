dbbeeb6d537c7b524ca2e82c4e721766bc67a3e8
Denisa Marian <denisacristinamarian@yahoo.com>
2020-08-05 14:15:27 +0300

    Added service, pages, navigation

--- cinemaAppFE/src/app/home/home.component.ts
+++ cinemaAppFE/src/app/home/home.component.ts
@@ -1,20 +1,38 @@
+<<<<<<< HEAD
 import { MovieService } from './../movie.service';
 import { Component, OnInit } from '@angular/core';
+=======
+import { MovieComponent } from './../movie/movie.component';
+import { MovieService } from './../movie.service';
+import { Component, OnInit, Input } from '@angular/core';
+>>>>>>> f4d2c61... Added service, pages, navigation
 import { Movie } from '../movie';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+<<<<<<< HEAD
   providers: [MovieService]
 })
 export class HomeComponent implements OnInit {
   movies: Movie[];
   constructor(private movieService: MovieService) { }
+=======
+  providers: [MovieService,MovieComponent],
+})
+export class HomeComponent implements OnInit {
+  movies: Movie[];
+  constructor(private movieService: MovieService,private movieComponent:MovieComponent) { }
+>>>>>>> f4d2c61... Added service, pages, navigation
 
   ngOnInit() { this.getMovieList(); }
 
   getMovieList(): void {
+<<<<<<< HEAD
     this.movieService.getMovies().subscribe(movies => (this.movies = movies));
+=======
+    this.movieService.getMovies().subscribe(movies =>(this.movies = movies));
+>>>>>>> f4d2c61... Added service, pages, navigation
   }
 }
--- cinemaAppFE/src/app/movie/movie.component.ts
+++ cinemaAppFE/src/app/movie/movie.component.ts
@@ -1,9 +1,19 @@
+<<<<<<< HEAD
 import { Component } from '@angular/core';
 
+=======
+import { Movie } from './../movie';
+import { Component,Input } from '@angular/core';
+>>>>>>> f4d2c61... Added service, pages, navigation
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
 export class MovieComponent {
+<<<<<<< HEAD
+=======
+ @Input() movie:Movie;
+
+>>>>>>> f4d2c61... Added service, pages, navigation
 }
