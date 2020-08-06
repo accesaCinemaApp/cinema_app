2d69b249f5a84c99f4b0900992ad8dc5e9e84e0a
Denisa Marian <52399410+denisamarian@users.noreply.github.com>
2020-08-06 17:32:26 +0300

    Update movie.component.ts

--- cinemaAppFE/src/app/movie/movie.component.ts
+++ cinemaAppFE/src/app/movie/movie.component.ts
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-
-=======
 import { Movie } from './../movie';
 import { Component,Input } from '@angular/core';
->>>>>>> f4d2c61... Added service, pages, navigation
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
 export class MovieComponent {
-<<<<<<< HEAD
-=======
  @Input() movie:Movie;
 
->>>>>>> f4d2c61... Added service, pages, navigation
 }
