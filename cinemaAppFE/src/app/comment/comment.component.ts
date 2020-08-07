747fc5576eb74aca1c69940b76b2d5f952a69959
Bianca-Simoca <68690320+Bianca-Simoca@users.noreply.github.com>
2020-08-07 14:48:57 +0300

    'Comment-section'

--- cinemaAppFE/src/app/comment/comment.component.ts
+++ cinemaAppFE/src/app/comment/comment.component.ts
@@ -1,52 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpRequest } from '@angular/common/http';
-declare var $: any;
+
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css']
 })
 export class CommentComponent implements OnInit {
-
+ comments:any[]=[];
+ formName;
+ formDate;
+ formBody;
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-}
-function render(data){
-  var html="<div class='commentBox'><div class='leftPanelImg'></div><div class='rightPanel'><span>"+data.name+"</span> <div class='date'>"+data.date+"</div><p>"+data.body+"</p></div><div class='clear'></div></div>";
-  
-  $('#container').append(html);
-}
-
-$(document).ready(function(){
-  var comment=[];
-
-  if(!localStorage.commentData){
+    /*$('#container').append();*/
+    if(!localStorage.commentData){
       localStorage.commentData=[];
   } else{
-        comment=JSON.parse(localStorage.commentData);
+        this.comments=JSON.parse(localStorage.commentData);
   }
   
-  for(var i=0;i<comment.length;i++){
-     render(comment[i]);
-       
-  }
-  $('#addComment').click(function(){
-       var addObj={
-           "name": $('#name').val(),
-           "date": $('#date').val(),
-           "body": $('#bodyText').val()
-       }
-     
-      comment.push(addObj);
-      localStorage.commentData= JSON.stringify(comment);
-      render(addObj);
-      $('#name').val('');
-      $('#date').val('dd/mm/yyyy');
-      $('#bodyText').val('');
+  /*for(var i=0;i<this.comments.length;i++){
+     render(this.comments[i]);  
+  }*/
+} 
+addComment() {
+  
+  let comment = {name: this.formName,
+  date: this.formDate,
+  body: this.formBody};
+  
+  this.comments.push(comment);
+  localStorage.commentData = JSON.stringify(this.comments);
+  } 
+}
+  
 
-      
-  });
-});
+
